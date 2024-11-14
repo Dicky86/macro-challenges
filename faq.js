65fc8917b7fa2dbc97f352723f15751c9d91075b
Moh. Dicky Purnama <114645106+Dicky86@users.noreply.github.com>
2024-11-14 11:10:22 +0700

    Delete faq.js

--- faq.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.faq-item').forEach(item => {
-    item.addEventListener('click', () => {
-        const content = item.nextElementSibling;
-        if (content && content.classList.contains('faq-content')) {
-            item.classList.toggle('active');
-            content.style.display = content.style.display === 'block' ? 'none' : 'block';
-        }
-    });
-});
